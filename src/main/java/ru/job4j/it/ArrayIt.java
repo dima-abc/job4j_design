fe5dd5a4fe225d7b420bb825c6aa78c19ce857a7
dmitryjob4j <haoos@inbox.ru>
2021-10-15 23:30:28 +0300

    2.1.1 Iterator.1. Что такое итератор.

---
+++ src/main/java/ru/job4j/it/ArrayIt.java
@@ -0,0 +1,34 @@
+package ru.job4j.it;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+/**
+ * 2.1.1 Iterator
+ * 1. Что такое итератор.help
+ *
+ * @author Dima_Nout
+ * @version 1
+ * @since 15.10.2021
+ */
+public class ArrayIt implements Iterator<Integer> {
+    private final int[] data;
+    private int point = 0;
+
+    public ArrayIt(int[] data) {
+        this.data = data;
+    }
+
+    @Override
+    public boolean hasNext() {
+        return point < data.length;
+    }
+
+    @Override
+    public Integer next() {
+        if (!hasNext()) {
+            throw new NoSuchElementException();
+        }
+        return data[point++];
+    }
+}
---
+++ src/main/java/ru/job4j/it/BackwardArrayIt.java
@@ -0,0 +1,36 @@
+package ru.job4j.it;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+/**
+ * 2.1.1 Iterator
+ * 1. Что такое итератор.
+ * Задание. Итератор должен отдавать элементы массива в обратном порядке.
+ *
+ * @author Dima_Nout
+ * @version 1
+ * @since 15.10.2021
+ */
+public class BackwardArrayIt implements Iterator<Integer> {
+    private int[] data;
+    private int point;
+
+    public BackwardArrayIt(int[] data) {
+        this.data = data;
+        point = data.length - 1;
+    }
+
+    @Override
+    public boolean hasNext() {
+        return point >= 0;
+    }
+
+    @Override
+    public Integer next() {
+        if (!hasNext()) {
+            throw new NoSuchElementException();
+        }
+        return data[point--];
+    }
+}
