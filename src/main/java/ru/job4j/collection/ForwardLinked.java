76b1b7bc15b8330c445b004b5bc176bef0b6c34c
dmitryjob4j <haoos@inbox.ru>
2021-10-25 01:17:16 +0300

    2.1.3.List.3.Удалить head в односвязном списке.[#51424#127220]

---
+++ src/main/java/ru/job4j/collection/ForwardLinked.java
@@ -0,0 +1,90 @@
+package ru.job4j.collection;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+/**
+ * 2.1.3. List
+ * 3. Удалить head в односвязном списке. [#51424 #127220]
+ *
+ * @param <T> type.
+ * @author Dima_Nout
+ * @version 1
+ * @since 25.10.2021
+ */
+public class ForwardLinked<T> implements Iterable<T> {
+    private Node<T> head;
+
+    /**
+     * Добавление нового элемента в конец списка.
+     *
+     * @param value Новый элемент
+     */
+    public void add(T value) {
+        Node<T> node = new Node<>(value, null);
+        if (head == null) {
+            head = node;
+            return;
+        }
+        Node<T> tail = head;
+        while (tail.next != null) {
+            tail = tail.next;
+        }
+        tail.next = node;
+    }
+
+    /**
+     * Удаление первого элемента в односвязном списке.
+     */
+    public T deleteFirst() {
+        if (head == null) {
+            throw new NoSuchElementException();
+        }
+        T value = head.value;
+        head = head.next;
+        return value;
+    }
+
+    /**
+     * Итератор
+     *
+     * @return iterator
+     */
+    @Override
+    public Iterator<T> iterator() {
+        return new Iterator<>() {
+            Node<T> node = head;
+
+            @Override
+            public boolean hasNext() {
+                return node != null;
+            }
+
+            @Override
+            public T next() {
+                if (!hasNext()) {
+                    throw new NoSuchElementException();
+                }
+                T value = node.value;
+                node = node.next;
+                return value;
+            }
+        };
+    }
+
+    /**
+     * Приватный класс описывает узел Node
+     * для односвязного списка.
+     *
+     * @param <T> type.
+     */
+    private static class Node<T> {
+        T value;
+        Node<T> next;
+
+        public Node(T value, Node<T> next) {
+            this.value = value;
+            this.next = next;
+        }
+    }
+}
---
+++ src/test/java/ru/job4j/collection/ForwardLinkedTest.java
@@ -0,0 +1,44 @@
+package ru.job4j.collection;
+
+import org.junit.Test;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+/**
+ * 2.1.3. List
+ * 3. Удалить head в односвязном списке.[#51424#127220]
+ * Test.
+ *
+ * @author Dima_Nout
+ * @version 1
+ * @since 25.10.2021
+ */
+public class ForwardLinkedTest {
+    @Test(expected = NoSuchElementException.class)
+    public void whenDeleteFirst() {
+        ForwardLinked<Integer> linked = new ForwardLinked<>();
+        linked.add(1);
+        linked.deleteFirst();
+        linked.iterator().next();
+    }
+
+    @Test(expected = NoSuchElementException.class)
+    public void whenDeleteEmptyLinked() {
+        ForwardLinked<Integer> linked = new ForwardLinked<>();
+        linked.deleteFirst();
+    }
+
+    @Test
+    public void whenMultiDelete() {
+        ForwardLinked<Integer> linked = new ForwardLinked<>();
+        linked.add(1);
+        linked.add(2);
+        assertThat(linked.deleteFirst(), is(1));
+        Iterator<Integer> it = linked.iterator();
+        assertThat(it.next(), is(2));
+    }
+}
