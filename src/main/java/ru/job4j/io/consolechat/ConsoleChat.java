61c8a1f3f4ba59d9d94b613031e14e0d86e57cfc
dmitryjob4j <haoos@inbox.ru>
2021-11-27 21:17:23 +0300

    2.2.1.Ввод-вывод.6.Кодировка.[#862 #127257]

--- src/main/java/ru/job4j/io/consolechat/ConsoleChat.java
+++ src/main/java/ru/job4j/io/consolechat/ConsoleChat.java
@@ -1,8 +1,10 @@
 package ru.job4j.io.consolechat;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.InputStreamReader;
+import ru.job4j.io.ArgsName;
+
+import java.io.*;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -36,13 +38,29 @@ public class ConsoleChat {
                 new InputStreamReader(System.in))) {
             String wordUser;
             String wordBot;
+            String muwBot = "";
+            List<String> botPhrase = readPhrases();
             System.out.printf("Команды для чата %s, %s, %s %n", STOP, CONTINUE, OUT);
             System.out.println("Введите текст для общения в чате");
             do {
+                System.out.print("-->");
                 wordUser = in.readLine();
-
-            } while (true);
-
+                this.logChat.add("User: ".concat(wordUser));
+                wordUser = wordUser.toLowerCase();
+                if (wordUser.equals(STOP)) {
+                    muwBot = STOP;
+                    continue;
+                }
+                if (wordUser.equals(CONTINUE)) {
+                    muwBot = CONTINUE;
+                }
+                if (!muwBot.equals(STOP) && !wordUser.equals(OUT)) {
+                    wordBot = generatePhrases(botPhrase);
+                    this.logChat.add("Bot: ".concat(wordBot));
+                    System.out.printf("Bot: %s%n", wordBot);
+                }
+            } while (!wordUser.equals(OUT));
+            saveLog(this.logChat);
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -56,7 +74,7 @@ public class ConsoleChat {
     private List<String> readPhrases() {
         List<String> result = new ArrayList<>();
         try (BufferedReader inFile = new BufferedReader(
-                (new FileReader(this.botAnswers)))) {
+                (new FileReader(this.botAnswers, StandardCharsets.UTF_8)))) {
             inFile.lines().forEach(result::add);
         } catch (Exception e) {
             e.printStackTrace();
@@ -65,15 +83,35 @@ public class ConsoleChat {
     }
 
     /**
+     * Генерация случайно фразы.
+     *
+     * @param phrases List
+     * @return String
+     */
+    private String generatePhrases(List<String> phrases) {
+        int rnd = (int) (Math.random() * phrases.size());
+        return phrases.get(rnd);
+    }
+
+    /**
      * Сохраняет лог чата в файл.
      *
-     * @param log List.
+     * @param logChat List.
      */
-    private void saveLog(List<String> log) {
-
+    private void saveLog(List<String> logChat) {
+        try (PrintWriter out = new PrintWriter(
+                new FileWriter(this.logPath, StandardCharsets.UTF_8, true))) {
+            for (String log : this.logChat) {
+                out.printf("%s%n", log);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     public static void main(String[] args) {
-
+        ArgsName argsName = ArgsName.of(args);
+        ConsoleChat consoleChat = new ConsoleChat(argsName.get("log"), argsName.get("phrase"));
+        consoleChat.run();
     }
 }
