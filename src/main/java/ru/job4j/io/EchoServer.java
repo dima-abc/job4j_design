5d6b0408739d51c41f3d514b14e637eea000bef8
dmitryjob4j <haoos@inbox.ru>
2021-11-29 17:33:09 +0300

    2.2.2.Socket.0.Что такое Socket?

---
+++ src/main/java/ru/job4j/io/EchoServer.java
@@ -0,0 +1,38 @@
+package ru.job4j.io;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+/**
+ * 2.2.2. Socket
+ * 0. Что такое Socket?
+ *
+ * @author Dmitry
+ * @since 29.11.2021
+ */
+public class EchoServer {
+    public static void main(String[] args) throws IOException {
+        try (ServerSocket server = new ServerSocket(9000)) {
+            while (!server.isClosed()) {
+                Socket socket = server.accept();
+                try (OutputStream out = socket.getOutputStream();
+                     BufferedReader in = new BufferedReader(
+                             new InputStreamReader(socket.getInputStream()))) {
+                    out.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
+                    for (String str = in.readLine(); str != null && !str.isEmpty(); str = in.readLine()) {
+                        System.out.println(str);
+                        if (str.contains("?msg=Bye")) {
+                            server.close();
+                            break;
+                        }
+                    }
+                    out.flush();
+                }
+            }
+        }
+    }
+}
