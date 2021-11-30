86f8e0d2f9507fa6eb905d2cce6a3e92cd7c9a3d
dmitryjob4j <haoos@inbox.ru>
2021-11-30 14:45:52 +0300

    2.2.3.Логирование.1.Log4j.Логирование системе.[#95335#127253]

---
+++ src/main/java/ru/job4j/io/UsageLog4j.java
@@ -0,0 +1,20 @@
+package ru.job4j.io;
+
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
+
+/**
+ * 2.2.3. Логирование
+ * 1. Log4j. Логирование системы.[#95335#127253]
+ */
+public class UsageLog4j {
+    private static final Logger LOG = LogManager.getLogger(UsageLog4j.class.getName());
+
+    public static void main(String[] args) {
+        LOG.trace("trace message");
+        LOG.debug("debug message");
+        LOG.info("info message");
+        LOG.warn("warn message");
+        LOG.error("error message");
+    }
+}
