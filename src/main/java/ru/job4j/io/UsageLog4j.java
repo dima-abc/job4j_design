4b6d35799a9520b2226c344727d1a5f535912c39
dmitryjob4j <haoos@inbox.ru>
2021-11-30 15:56:21 +0300

    2.2.3.Логирование.3. Slf4j - вывод переменных.

--- src/main/java/ru/job4j/io/UsageLog4j.java
+++ src/main/java/ru/job4j/io/UsageLog4j.java
@@ -4,8 +4,13 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 /**
- * 2.2.3. Логирование
- * 1. Log4j. Логирование системы.[#95335#127253]
+ * 2.2.3. Логирование.
+ * 1. Log4j. Логирование системы.[#95335#127253].
+ * 2. Simple Loggin Facade 4 Java. [#268849 #127252]
+ * 3. Slf4j - вывод переменных.
+ *
+ * @author Dmitry
+ * @since 30.11.2021
  */
 public class UsageLog4j {
     private static final Logger LOG = LoggerFactory.getLogger(UsageLog4j.class.getName());
@@ -16,5 +21,20 @@ public class UsageLog4j {
         LOG.info("info message");
         LOG.warn("warn message");
         LOG.error("error message");
+        byte first = 8;
+        short second = 16;
+        int third = 32;
+        long fourth = 64;
+        float fifth = 32.32F;
+        double sixth = 64.64D;
+        char seventh = 'c';
+        boolean eighth = true;
+        LOG.debug(
+                "User info byte first : {}, short second : {}, int third : {}, long fourth : {}",
+                first, second, third, fourth);
+        LOG.debug(
+                "User info byte float fifth : {}, double sixth : {}, char seventh : {}, boolean eighth : {}",
+                fifth, sixth, seventh, eighth);
+
     }
 }
