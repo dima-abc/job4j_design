7ae4d72fa384414d63361bed00036b34c0096cba
dmitryjob4j <haoos@inbox.ru>
2021-11-30 14:52:52 +0300

    2.2.3.Логирование.2. Simple Loggin Facade 4 Java. [#268849 #127252]

--- src/main/java/ru/job4j/io/UsageLog4j.java
+++ src/main/java/ru/job4j/io/UsageLog4j.java
@@ -1,14 +1,14 @@
 package ru.job4j.io;
 
-import org.apache.log4j.LogManager;
-import org.apache.log4j.Logger;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * 2.2.3. Логирование
  * 1. Log4j. Логирование системы.[#95335#127253]
  */
 public class UsageLog4j {
-    private static final Logger LOG = LogManager.getLogger(UsageLog4j.class.getName());
+    private static final Logger LOG = LoggerFactory.getLogger(UsageLog4j.class.getName());
 
     public static void main(String[] args) {
         LOG.trace("trace message");
