db4d8ed061cd95ed8e2b6db460191f353a60ca76
dmitryjob4j <haoos@inbox.ru>
2021-12-02 18:00:16 +0300

    2.2.4.Сериализаци..4.JAXB. Преобразование XML в POJO.[#315063]

--- src/main/java/ru/job4j/serialization/json/Contact.java
+++ src/main/java/ru/job4j/serialization/json/Contact.java
@@ -1,15 +1,25 @@
 package ru.job4j.serialization.json;
 
+import javax.xml.bind.annotation.XmlAttribute;
+import javax.xml.bind.annotation.XmlRootElement;
+
 /**
  * 2.2.4. Сериализация
  * 2. Формат JSON [#313164]
+ * 4. JAXB. Преобразование XML в POJO. [#315063]
  * Модель данных Contact.
  *
  * @author Dmitry
- * @since 01.12.2021
+ * @since 02.12.2021
  */
+
+@XmlRootElement(name = "contact")
 public class Contact {
-    private final String phone;
+    @XmlAttribute
+    private String phone;
+
+    public Contact() {
+    }
 
     public Contact(String phone) {
         this.phone = phone;
--- src/main/java/ru/job4j/serialization/json/Person.java
+++ src/main/java/ru/job4j/serialization/json/Person.java
@@ -1,20 +1,33 @@
 package ru.job4j.serialization.json;
 
 import java.util.Arrays;
+import javax.xml.bind.annotation.*;
+
 
 /**
  * 2.2.4. Сериализация
  * 2. Формат JSON [#313164]
+ * 4. JAXB. Преобразование XML в POJO. [#315063]
  * Модель данных Person.
  *
  * @author Dmitry
- * @since 01.12.2021
+ * @since 02.12.2021
  */
+@XmlRootElement(name = "person")
+@XmlAccessorType(XmlAccessType.FIELD)
 public class Person {
-    private final boolean sex;
-    private final int age;
-    private final Contact contact;
-    private final String[] statuses;
+    @XmlAttribute
+    private boolean sex;
+    @XmlAttribute
+    private int age;
+    @XmlAttribute
+    private Contact contact;
+    @XmlElementWrapper(name = "statuses")
+    @XmlElement(name = "statuses")
+    private String[] statuses;
+
+    public Person() {
+    }
 
     public Person(boolean sex, int age, Contact contact, String[] statuses) {
         this.sex = sex;
