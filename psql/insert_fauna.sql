df6554066f623747e787b82900cab5f7a5bdc730
dmitryjob4j <haoos@inbox.ru>
2021-12-17 15:39:29 +0300

    2.3.2.Query.0.Select[#6861]

---
+++ psql/insert_fauna.sql
@@ -0,0 +1,11 @@
+insert into fauna(name, avg_age, discovery_date) values ('fish sword', 18250, '01.01.1585');
+insert into fauna(name, avg_age) values ('fish hammer', 25000);
+insert into fauna(name, avg_age, discovery_date) values ('fish parrot', 3600, '20.07.1885');
+insert into fauna(name, avg_age, discovery_date) values ('locust insect', 50, '01.02.1125');
+insert into fauna(name, avg_age) values ('insect butterfly',100);
+insert into fauna(name, avg_age, discovery_date) values('insect butterfly', 30, '12.10.1955');
+insert into fauna(name, avg_age) values('elephant', 36500);
+insert into fauna(name, avg_age, discovery_date) values('Wolf', 5475, '09.09.1912');
+insert into fauna(name, avg_age, discovery_date) values('monkey', 10500, '12.12.1993');
+insert into fauna(name, avg_age) values ('bear', 100000);
+
