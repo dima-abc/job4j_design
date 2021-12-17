f3ed59f0c3088c0196000d08555635b7d7a590fa
dmitryjob4j <haoos@inbox.ru>
2021-12-17 15:17:48 +0300

    2.3.2.Query.0.Select[#6861]

---
+++ psql/select_fauna.sql
@@ -0,0 +1,4 @@
+SELECT * FROM fauna WHERE name LIKE '%fish%';
+SELECT * FROM fauna WHERE avg_age<21000 AND avg_age>10000;
+SELECT * FROM fauna WHERE discovery_date is NULL;
+SELECT * FROM fauna WHERE discovery_date>'01.01.1950';
