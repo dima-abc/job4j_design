34b83bb757de065c2dc713f419f9a1a47e26f772
dmitryjob4j <haoos@inbox.ru>
2022-01-12 22:29:50 +0300

    2.3.7.Контрольные вопросы.2.SQL Select by company[#2760]исправил замечания

--- psql/2. SQL Select by company [#2760].sql
+++ psql/2. SQL Select by company [#2760].sql
@@ -20,7 +20,7 @@ insert into company(id, name) values(1, 'ООО "Рога и Капыта"'), (2
 insert into person(id, name, company_id) values(1, 'Fedora', 1), (2, 'Egora', 1),
 (3, 'Tomara', 2), (4, 'Dorado',2), (5, 'Masha', 3), (6, 'Dasha', 3), 
 (7, 'Sveta', 4), (8, 'Diana', 4), (9, 'Ira', 5), (10, 'Galya', 5), 
-(11, 'Faina', 2), (12, Alina, 2), (13, 'Denis',5), (14, 'Sasha', 5); 
+(11, 'Faina', 2), (12, 'Alina', 2), (13, 'Denis',5), (14, 'Sasha', 5);
 /*1-В одном запросе получить
 - имена всех person, которые не состоят в компании с id = 5;
 - название компании для каждого человека.*/
@@ -33,4 +33,13 @@ inner join person p
 on p.company_id=c.id
 group by c.name)
 select * from counter where counter.cou=(select max(counter.cou) from counter);
-
+--вариант 2 по подсказке Lana Sergeeva--
+select c.name as COMPANY_NAME, count(*) as COUNT_PERSON from company c
+inner join person p
+on p.company_id=c.id
+group by c.name
+having count(p.id) = (select count(*) from person a
+                        inner join person b
+                        on a.company_id=b.company_id
+                        group by a.id
+                        order by a.id desc limit 1)
